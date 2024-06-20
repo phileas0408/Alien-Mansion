d2803e053224e8686a08ef06b810741ef7107020
phileas0408 <phileas0408@users.noreply.github.com>
2024-06-20 10:19:40 +0200

    asset fond

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -34,13 +34,6 @@ public class Player : MonoBehaviour
             }
         }
 
-        if (Input.GetButton("Down"))
-        {
-            transform.localScale = new Vector3(1,0.5f,1);
-        }else{
-            transform.localScale = new Vector3(1,1,1);
-        }
-
         character.Move(direction * Time.deltaTime);
     }
 
